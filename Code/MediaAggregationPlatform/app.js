9aae15136d4a674623e5ebca0f009949c56ac17b
Derek Fritz <derekfritz10@gmail.com>
2018-11-11 17:27:06 -0600

    Removed pug files, added media source selection, began work on NY Times API

--- Code/MediaAggregationPlatform/app.js
+++ Code/MediaAggregationPlatform/app.js
@@ -2,8 +2,10 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var twitter = require('./twitterAPI');
+var nytimes = require('./nytimesAPI');
 
 //var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
@@ -21,14 +23,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Retrieve and display tweets
+// Retrieve and display news & media
 tweetArray = twitter.getTweets();
+//newsArray = nytimes.getNews();
 
 app.get('/', function(req, res) {
-    res.render('index.ejs', {page: 'Home', menuId: 'home',
-        topic1: tweetArray[0][0], topic2: tweetArray[0][1], topic3: tweetArray[0][2],
-        tweets1: tweetArray[1].slice(0,3), tweets2: tweetArray[1].slice(3,6), tweets3: tweetArray[1].slice(6,9) });
+    res.render('indexHome.ejs', {page: 'Home', menuId: 'home'});
+});
+app.post('/', function(req, res) {
+    var selectVal = req.body.list;
+    switch(selectVal) {
+        case 'twitter':
+            res.render('index.ejs', {page: 'Home', menuId: 'home',
+                topic1: tweetArray[0][0], topic2: tweetArray[0][1], topic3: tweetArray[0][2],
+                tweets1: tweetArray[1].slice(0,3), tweets2: tweetArray[1].slice(3,6), tweets3: tweetArray[1].slice(6,9)
+            });
+            break;
+        case 'nytimes':
+            break;
+        case 'youtube':
+            break;
+        case 'spotify':
+            break;
+    }
 });
 app.listen(3000, function(){
     console.log("Listening on http://localhost:3000");
